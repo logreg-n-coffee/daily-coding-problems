14dce194329279c7f1bf4a0b224abc7cf683fcca
Rui Hu <hurui141@gmail.com>
2022-11-05 23:58:23 -0700

    Add O(n) solution for question 16

---
+++ code/16-record-the-last-n-order-ids.js
@@ -0,0 +1,59 @@
+/**
+ * You run an e-commerce website and want to record the last N order ids in a log. 
+ * 
+ * Implement a data structure to accomplish this, with the following API:
+ * 
+ * record(order_id): adds the order_id to the log
+ * get_last(i): gets the ith last element from the log. i is guaranteed to be smaller than or equal to N.
+ * 
+ * You should be as efficient with time and space as possible.
+ */
+
+class Log {
+    #log;  // declare private field
+
+    constructor(n) {
+        this.#log = [];  // initialize an array 
+        this.n = n;  // use variable n to limit the max length of the array 
+    }
+
+    // add the orderId to the log
+    record(orderId) {
+        if (this.#log.length >= this.n) {
+            this.#log.splice(this.#log.length - 1, 1);  // O(n)
+        }
+        this.#log.unshift(orderId);
+    }
+
+    // get the i-th last element - 
+    // since the lastly added element goes to the beginning, 
+    // it literally retrieves the beginning of the i-th element
+    getLast(i) {
+        return this.#log[i - 1];
+    }
+
+    // print everything in the private log array
+    print() {
+        this.#log.forEach((element, index) => console.log('element, index', element, index));
+    }
+
+}
+
+// driver code
+console.log('---O(n)---');
+const log = new Log(5);
+for (let i = 0; i < 6; i++) {
+    log.record(i);
+}
+log.record(100);
+log.record(200);
+log.record(300);
+log.record(400);
+log.record(500);
+log.record(600);
+console.log(log.getLast(1));
+log.print();
+
+
+// O(1) constant time solution - build a circular buffer
+
