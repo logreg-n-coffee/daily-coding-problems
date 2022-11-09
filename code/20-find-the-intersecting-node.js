5e3762c5cb75d0bc1146c779f13a882a9c494d5b
Rui Hu <hurui141@gmail.com>
2022-11-09 10:29:53 -0700

    Add solution for question 20

---
+++ code/20-find-the-intersecting-node.js
@@ -0,0 +1,198 @@
+/**
+ * Given two singly linked lists that intersect at some point, find the intersecting node. 
+ * The lists are non-cyclical.
+ * 
+ * For example, given A = 3 -> 7 -> 8 -> 10 and B = 99 -> 1 -> 8 -> 10, return the node with value 8.
+ * 
+ * In this example, assume nodes with the same value are the exact same node objects.
+ * Do this in O(M + N) time (where M and N are the lengths of the lists) and constant space.
+ * 
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val) {
+ *         this.val = val;
+ *         this.next = null;
+ *     }
+ * }
+ */
+
+// there are two cases to think about:
+// 1. Two linked lists have the same length - move the pointer all the way to the end until it reaches the end
+// 2. Two linked lists do not have the same length - put t
+
+const getIntersectionNode = (headA, headB) => {
+    // boundary check
+    if (headA === null || headB === null) return null;
+
+    let p1 = headA;
+    let p2 = headB;
+
+    while (p1 !== p2) {
+        // for the end of first iteration, we just reset the pointer to the head of another linkedlist
+        p1 = p1 === null ? headB : p1.next;
+        p2 = p2 === null ? headA : p2.next;
+    }
+
+    return p1;
+};
+
+// n -> length of linked list A
+// m -> length of linked list B
+// time: O(n + m)
+// space: O(1)
+
+// illustration:
+
+/*
+Case 1 (Have Intersection & Same Len):
+
+       a
+A:     a1 → a2 → a3
+                   ↘
+                     c1 → c2 → c3 → null
+                   ↗            
+B:     b1 → b2 → b3
+       b
+            a
+A:     a1 → a2 → a3
+                   ↘
+                     c1 → c2 → c3 → null
+                   ↗            
+B:     b1 → b2 → b3
+            b
+                 a
+A:     a1 → a2 → a3
+                   ↘
+                     c1 → c2 → c3 → null
+                   ↗            
+B:     b1 → b2 → b3
+                 b
+A:     a1 → a2 → a3
+                   ↘ a
+                     c1 → c2 → c3 → null
+                   ↗ b            
+B:     b1 → b2 → b3
+Since a == b is true, end loop while(a != b), return the intersection node a = c1.
+
+Case 2 (Have Intersection & Different Len):
+
+            a
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3 → null
+                   ↗            
+B:     b1 → b2 → b3
+       b
+                 a
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3 → null
+                   ↗            
+B:     b1 → b2 → b3
+            b
+A:          a1 → a2
+                   ↘ a
+                     c1 → c2 → c3 → null
+                   ↗            
+B:     b1 → b2 → b3
+                 b
+A:          a1 → a2
+                   ↘      a
+                     c1 → c2 → c3 → null
+                   ↗ b           
+B:     b1 → b2 → b3
+A:          a1 → a2
+                   ↘           a
+                     c1 → c2 → c3 → null
+                   ↗      b           
+B:     b1 → b2 → b3
+A:          a1 → a2
+                   ↘                a = null, then a = b1
+                     c1 → c2 → c3 → null
+                   ↗           b           
+B:     b1 → b2 → b3
+A:          a1 → a2
+                   ↘ 
+                     c1 → c2 → c3 → null
+                   ↗                b = null, then b = a1 
+B:     b1 → b2 → b3
+       a
+            b         
+A:          a1 → a2
+                   ↘ 
+                     c1 → c2 → c3 → null
+                   ↗
+B:     b1 → b2 → b3
+            a
+                 b         
+A:          a1 → a2
+                   ↘ 
+                     c1 → c2 → c3 → null
+                   ↗ 
+B:     b1 → b2 → b3
+                 a
+A:          a1 → a2
+                   ↘ b
+                     c1 → c2 → c3 → null
+                   ↗ a
+B:     b1 → b2 → b3
+Since a == b is true, end loop while(a != b), return the intersection node a = c1.
+
+Case 3 (Have No Intersection & Same Len):
+
+       a
+A:     a1 → a2 → a3 → null
+B:     b1 → b2 → b3 → null
+       b
+            a
+A:     a1 → a2 → a3 → null
+B:     b1 → b2 → b3 → null
+            b
+                 a
+A:     a1 → a2 → a3 → null
+B:     b1 → b2 → b3 → null
+                 b
+                      a = null
+A:     a1 → a2 → a3 → null
+B:     b1 → b2 → b3 → null
+                      b = null
+Since a == b is true (both refer to null), end loop while(a != b), return a = null.
+
+Case 4 (Have No Intersection & Different Len):
+
+       a
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+       b
+            a
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+            b
+                 a
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+                 b
+                      a
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+                      b = null, then b = a1
+       b                   a = null, then a = b1
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+            b                   
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+       a
+                 b
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+            a
+                      b
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+                 a
+                           b = null
+A:     a1 → a2 → a3 → a4 → null
+B:     b1 → b2 → b3 → null
+                      a = null
+*/
