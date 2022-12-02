9afc1b0a716f1b7da4f249c13266564a79628a15
Rui Hu <hurui141@gmail.com>
2022-12-02 00:57:13 -0700

    Add solution to q 42 - Update prev solutions

--- code/42-subset-adding-up-to-k.js
+++ code/42-subset-adding-up-to-k.js
@@ -8,12 +8,49 @@ Integers can appear more than once in the list. You may assume all numbers in th
 For example, given nums = [12, 1, 61, 5, 9, 2] and k = 24, return [12, 9, 2, 1] since it sums up to 24.
 */
 
-const subsetSumRecursive = (nums, k) => {
+const subsetSum = (nums, k) => {
+    const n = nums.length;
+    const dp = Array(n + 1).fill().map(() => Array(k + 1).fill(null));  // 2d array: dp[n + 1][k + 1]
 
+    // initialize first element in each row as an empty array - first column as an empty array
+    for (let i = 0; i < n + 1; i++) {
+        dp[i][0] = []; 
+    }
+
+    for (let i = 1; i < n + 1; i++) {
+        for (let j = 1; j < k + 1; j++) {
+            // get the last element in the nums array we are looking at nums[i - 1]
+            const last = nums[i - 1];
+            // IF last is greater than j (current k value), for sure we can't make j with the nums.slice(j) (j included)
+            if (last > j) {
+                // copy whatever we have from dp[i - 1][j]
+                dp[i][j] = dp[i - 1][j];
+            } else {
+                // ELSE we can still make j using the last value
+                // if we can make j without last and dp[i - 1][j] is not null
+                if (dp[i - 1][j] !== null) {
+                    dp[i][j] = dp[i - 1][j];
+                } else if (dp[i - 1][j - last] !== null) {
+                    // if we can't make j without last, 
+                    // then check if we can make j with last value by looking at (possible) array at dp[i - 1][j - last]
+                    // if we can: copy over the array and append the last element to the array
+                    dp[i][j] = dp[i - 1][j - last].concat(last);
+                } else {
+                    // we can't make it with or without j
+                    dp[i][j] = null;
+                }
+            }
+        }
+    }
+
+    return dp[n][k];
 };
 
 // Driver Code
-console.log(subsetSumRecursive([12, 1, 61, 5, 9, 2], 24));
+console.log(subsetSum([12, 1, 61, 5, 9, 2], 24));
+console.log(subsetSum([1, 2, 4], 3))
+
+// Complexities: TC & SC: O(k * n)
 
 /*
 Examples:
@@ -62,6 +99,5 @@ Solutions:
                 (0, 0)  (0, 2)
                 {True} {False}    
     2) Dynamic programming
-
-
+        See above solution.
 */
