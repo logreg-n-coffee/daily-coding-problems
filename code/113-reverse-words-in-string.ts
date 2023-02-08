d2211b9c135dcbb0491e04c96a6d727268871a85
Rui Hu <hurui141@gmail.com>
2023-02-08 14:48:36 -0700

    Add solution to q 113

---
+++ code/113-reverse-words-in-string.ts
@@ -0,0 +1,18 @@
+/*
+
+Given a string of words delimited by spaces, reverse the words in string. 
+For example, given "hello world here", return "here world hello"
+
+Follow-up: given a mutable string representation, can you perform this operation in-place?
+
+ */
+
+(() => { 
+    // solve the problem with string reconstruction
+    const reverse = (sentence: string): string => { 
+        const words = sentence.split(' ');
+        return words.reduce((acc, word) =>  word + ' ' + acc, '');
+    };
+
+    console.log(reverse('hello world here'));
+})();
